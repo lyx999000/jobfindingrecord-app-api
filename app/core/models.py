3a647913464d22a6db4b0bd411b3959ffe61c759
lyx999000 <yexinlu93@gmail.com>
2020-08-03 13:46:38 +1000

    admin suport custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
 
 
 class UserManager(BaseUserManager):
-     
+
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Invalid email address')
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
         user.is_staff = True
